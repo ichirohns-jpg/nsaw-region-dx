24d5a97a59d522f0be138f9503c61eee5c64f5ac
ichirohns-jpg <ichiro.hns@gmail.com>
2026-05-24 20:10:44 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+window.APP_CONFIG = {
+
+GAS_URL:
+"https://script.google.com/macros/s/AKfycbxNK_8iG01PJAECkqdEP412wQEA1XZ-vf4x5pUkOnUr98S2BR2Sjxp3wUmPFzbmHOdw/exec",
+
+AI_MODEL:
+"gpt-4o-mini",
+
+APP_NAME:
+"NSAW 地域DX"
+
+};
